0135ba2ee09b32f69b031c56a037efc49c36456c
mliu2003 <liumax2003@gmail.com>
2024-11-28 18:34:55 -0500

    initial graph visualization and structure

--- script.js
+++ script.js
@@ -1,19 +1,26 @@
-// D3 Scrollytelling Script
 const steps = d3.selectAll(".step");
 const svg = d3.select("svg");
 const width = svg.node().clientWidth;
 const height = svg.node().clientHeight;
 
-// Example visualization setup
-svg.append("circle")
+svg
+  .append("circle")
   .attr("cx", width / 2)
   .attr("cy", height / 2)
   .attr("r", 50)
   .style("fill", "steelblue");
 
+const dimensions = {
+  width: width / 2,
+  height: height / 2,
+};
+
+drawVegetablePrices(svg, dimensions);
+
 const updateVisualization = (stepIndex) => {
   console.log(`Step ${stepIndex + 1} active`);
-  svg.select("circle")
+  svg
+    .select("circle")
     .transition()
     .duration(500)
     .attr("r", 50 + stepIndex * 20)
@@ -45,29 +52,27 @@ steps.each(function () {
 // "Entity" attr has been reassigned to "name". Code field is also omitted as it is empty. Year and time has also been omitted as it is always 2010
 function greehouseGasDataPreprocessor(row) {
   return {
-    'name': row['Entity'],
-    'emissions_per_kg': row['GHG emissions per kilogram (Poore & Nemecek, 2018)']
-  }
+    name: row["Entity"],
+    emissions_per_kg: row["GHG emissions per kilogram (Poore & Nemecek, 2018)"],
+  };
 }
 
 // "Vegetable" attr has been reassigned to name. "RetailPriceUnit" is always per pound, so this has been omitted
 function vegetablePriceDataPreprocessor(row) {
   return {
-    'name': row['Vegetable'],
-    'form': row['Form'], 
-    'retail_price_per_lb': row['RetailPrice'],
-    'yield': row['Yield'],
-    'cup_equivalent_size': row['CupEquivalentSize'],
-    'cup_equivalent_price': row['CupEquivalentPrice']
-  }
+    name: row["Vegetable"],
+    form: row["Form"],
+    retail_price_per_lb: row["RetailPrice"],
+    yield: row["Yield"],
+    cup_equivalent_size: row["CupEquivalentSize"],
+    cup_equivalent_price: row["CupEquivalentPrice"],
+  };
 }
-
 
 // "Entity" attr has been reassigned to "name". Code field is also omitted as it is empty. Year has also been omitted as it is always 2010
 function landUseDataPreprocessor(row) {
   return {
-    'name': row['Entity'],
-    'land_use_per_kg': row['Land use per kilogram (Poore & Nemecek, 2018)']
-  }
+    name: row["Entity"],
+    land_use_per_kg: row["Land use per kilogram (Poore & Nemecek, 2018)"],
+  };
 }
-
