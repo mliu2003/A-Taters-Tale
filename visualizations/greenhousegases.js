0863b14cdad1e606f7b004b12087553bfeb4d743
Alyse Kwok <alysekwokk@gmail.com>
2024-12-01 16:02:26 -0500

    Add potato data identifier and change scrollytelling description

--- visualizations/greenhousegases.js
+++ visualizations/greenhousegases.js
@@ -3,6 +3,7 @@ function drawGreenhouseGasEmissions(svg, dimensions) {
     const padding = 50;
     const circleRadius = 40; 
     const columns = 3; 
+    const maxSize = 210
 
     d3.csv("data/greenhouse-gas-emissions-per-kilogram-of-food-product.csv").then((rawData) => {
         const gasEmissions = rawData.map((row) =>
@@ -13,7 +14,7 @@ function drawGreenhouseGasEmissions(svg, dimensions) {
 
         var radiusScale = d3.scaleSqrt()
             .domain([0, d3.max(gasEmissions, d => d.emissions_per_kg)])
-            .range([50, 200]);
+            .range([50, maxSize]);
         
         var cols_skipped = 0
         
@@ -27,7 +28,7 @@ function drawGreenhouseGasEmissions(svg, dimensions) {
                 col = (i + cols_skipped) % columns;
                 row = Math.floor((i + cols_skipped) / columns);
             } 
-            const x = col * (2 * circleRadius + padding) + circleRadius + padding;
+            const x = col * (2 * circleRadius + padding) + circleRadius + padding + 150;
             const y = row * (2 * circleRadius + padding) + circleRadius + padding;
             return { x, y };
             });
@@ -54,12 +55,29 @@ function drawGreenhouseGasEmissions(svg, dimensions) {
             .data(gasEmissions)
             .enter()
             .append("text")
-            .attr("x", (d, i) => positions[i].x + 100 / 2) // Centered horizontally
-            .attr("y", (d, i) => positions[i].y + 100 / 2) // Positioned below the image
+            .attr("x", (d, i) => positions[i].x + 100 / 2) 
+            .attr("y", (d, i) => positions[i].y + 100 / 2)
             .attr("text-anchor", "middle")
             .attr("font-size", "12px")
             .attr("fill", "black")
             .text(d => d.name);
+        
+        const potatoData = gasEmissions.find(d => d.name === "Potatoes");
+        const potatoIndex = gasEmissions.indexOf(potatoData);
+
+        if (potatoData) {
+            const potatoPosition = positions[potatoIndex];
+            const circleRadius = 30; 
+        
+            svg.append("circle")
+                .attr("cx", potatoPosition.x + 50) 
+                .attr("cy", potatoPosition.y + 50) 
+                .attr("r", circleRadius)
+                .attr("fill", "none")
+                .attr("stroke", "red")
+                .attr("stroke-width", 3);
+        }
+
 
 
     })
